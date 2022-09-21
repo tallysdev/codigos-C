e290cee6a7058f52c684a1d7ec58ca9855d3bfe8
tallys-aureliano <tallysaureliano@outlook.com>
2022-09-21 19:47:51 -0300

    main: testes

---
+++ leaed/searching/linear.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// int busca (int arr);
+
+int main(void) {
+
+ int arr[] = {0,1,2,3,4,5,6,7,8,9};    
+ int arry = sizeof(arr);
+ printf("%d",arry);
+    return 0;
+}
+
+// int busca(arr) {
+    
+ 
+    
+
+//     return 0;
+// }
