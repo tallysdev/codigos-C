72c6fd61676a815157e923258347333619b0675f
tallys-aureliano <tallysaureliano@outlook.com>
2022-09-21 21:42:07 -0300

    main: busca linear feita

--- leaed/searching/linear.c
+++ leaed/searching/linear.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-// int busca (int arr);
+int busca (int arr[], int, int);
 
 int main(void) {
 
  int arr[] = {0,1,2,3,4,5,6,7,8,9};    
- int arry = sizeof(arr);
- printf("%d",arry);
-    return 0;
+ int tam = sizeof(arr)/sizeof(arr[0]);
+ int num = 7;
+ int result = busca(arr, num, tam);
+ printf("%d", result);
+ return 0;
 }
 
-// int busca(arr) {
+int busca(int arr[], int num, int tam) {
     
- 
+    for (int i = 0; i < tam; i++)
+    {
+        if (arr[i] == num)
+        {
+            return 1;
+        }
     
+    }
 
-//     return 0;
-// }
+     return 0;
+ }
