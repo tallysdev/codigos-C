6e6ad9e94657338c07560d1b1537f1ee4586164e
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-12 17:16:24 -0300

    primeiras validacoes

--- cpf-vali.c
+++ cpf-vali.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-char verificar(char cpf[], int);
+char verificar(char* cpf, int);
 
-char verificar(char cpf[], int tam){
+char verificar(char* cpf, int tam){
     
-    if (tam<11)
-    {
-        return printf("CPF invalido");
+    for (int i = 0; i < tam; i++)
+    {   
+        if (cpf[i] == NULL)
+        {
+            return printf("CPF INVALIDO\n");
+        }
+        else if ( (cpf[i] > 45 || cpf[i] < 57) && cpf[i] == 47 )
+        {
+            return printf("CPF INVALIDO\n");
+
+        }
+        
+        /* code */
     }
     
-    return -1;
+    return 0;
 }
 
 int main(int argc, char const *argv[])
@@ -18,7 +28,7 @@ int main(int argc, char const *argv[])
     printf("Insira o CPF:");
     scanf("%s",cpf);
     getchar();
-    printf("%s", cpf);
+    // printf("%s", cpf);
     int tam = sizeof(cpf)/sizeof(cpf[0]);
     verificar(cpf,tam);
 
