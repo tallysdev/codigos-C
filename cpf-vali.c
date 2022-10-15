7ebbde0bf9d38b7f70d4b26aef040fe45b776620
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-15 19:31:43 -0300

    por hoje é só

--- cpf-vali.c
+++ cpf-vali.c
@@ -44,32 +44,36 @@ char verificar(char *cpf, int tam)
         {
             return printf("CPF INVALIDO\n");
         }
-    }
 
+    } 
+    
     else if (tam > 11)
     {
         int calc = 0;
         calc = cpf[0] * 10 + cpf[1] * 9 + cpf[2] * 8 + cpf[4] * 7 + cpf[5] * 6 + cpf[6] * 5 + cpf[8] * 4 + cpf[9] * 3 + cpf[10] * 2;
         int dv1 = calc % 11;
         int dv2 = (cpf[0] * 11 + cpf[1] * 10 + cpf[2] * 9 + cpf[4] * 8 + cpf[5] * 7 + cpf[6] * 6 + cpf[8] * 5 + cpf[9] * 4 + cpf[10] * 3 + cpf[12] * 2) % 11;
-
-        if ((dv1 < 2 && cpf[tam - 2] != '0') || (dv2 < 2 && cpf[tam - 2] != '0'))
+        char dc1, dc2; 
+        dc1 = (11 - dv1)+48;
+        dc2 = (11 - dv2)+48; 
+        if ((dv1 < 2 && cpf[12] != '0') || (dv2 < 2 && cpf[13] != '0'))
         {
             return printf("CPF a\n");
         }
-        else if ((dv1 >= 2 && cpf[tam - 2] == '0') || (dv2 >= 2 && cpf[tam - 2] == '0'))
+        else if ((dv1 >= 2 && cpf[12] == '0') || (dv2 >= 2 && cpf[13] == '0'))
         {
             return printf("CPF b\n");
         }
-        else if ((dv2 >= 2 && cpf[tam - 2] != 11 - dv2))
+        
+        else if ((dv1 >= 2 && cpf[12] != dc1) || (dv2 >= 2 && cpf[13] != dc2))
         {
             return printf("CPF c\n");
-        }
-        else
-        {
+        } 
+        else{
             return printf("CPF VÁLIDO");
         }
     }
+    
 
     return printf("CPF VÁLIDO");
 }
@@ -84,6 +88,8 @@ int main(int argc, char const *argv[])
     // printf("%s", cpf);
     int tam = strlen(cpf);
     printf("%d", tam);
+    printf("%c\n", cpf[12]);
+    printf("%c\n", cpf[13]);
     verificar(cpf, tam);
 
     return 0;
