cef2a5b7c66d416c59255a0eb460f7bdcd2da954
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-16 18:02:25 -0300

    validando todos os tipos

--- cpf-vali.c
+++ cpf-vali.c
@@ -31,18 +31,21 @@ char verificar(char *cpf, int tam)
         calc = (cpf[0] - 48 )* 10 + (cpf[1] - 48 )* 9 + (cpf[2] - 48 )* 8 + (cpf[3] - 48 )* 7 + (cpf[4] - 48 )* 6 + (cpf[5] - 48 )* 5 + (cpf[6] - 48 )* 4 + (cpf[7] - 48 )* 3 + (cpf[8] - 48 )* 2;
         int dv1 = calc % 11;
         int dv2 = ((cpf[0] - 48 )* 11 + (cpf[1] - 48 )* 10 + (cpf[2] - 48 )* 9 + (cpf[3] - 48 )* 8 + (cpf[4] - 48 )* 7 + (cpf[5] - 48 )* 6 + (cpf[6] - 48 )* 5 + (cpf[7] - 48 )* 4 + (cpf[8] - 48 )* 3 + (cpf[9] - 48 )* 2) % 11;
+        char dc1, dc2; 
+        dc1 = (11 - dv1) + 48;
+        dc2 = (11 - dv2) + 48;
 
         if ((dv1 < 2 && cpf[tam - 2] != '0') || (dv2 < 2 && cpf[tam - 2] != '0'))
         {
-            return printf("CPF INVALIDO\n");
+            return printf("CPF INVALIDOa\n");
         }
         else if ((dv1 >= 2 && cpf[tam - 2] == '0') || (dv2 >= 2 && cpf[tam - 2] == '0'))
         {
-            return printf("CPF INVALIDO\n");
+            return printf("CPF INVALIDOb\n");
         }
-        else if ((dv1 >= 2 && cpf[tam - 2] != 11 - dv1) || (dv2 >= 2 && cpf[tam - 2] != 11 - dv2))
+        else if ((dv1 >= 2 && cpf[tam - 2] != dc1) || (dv2 >= 2 && cpf[tam - 1] != dc2))
         {
-            return printf("CPF INVALIDO\n");
+            return printf("CPF INVALIDOc\n");
         }
 
     } 
@@ -58,8 +61,6 @@ char verificar(char *cpf, int tam)
         dc2 = (11 - dv2) + 48; 
         if ((dv1 < 2 && cpf[12] != '0') || (dv2 < 2 && cpf[13] != '0'))
         {   
-            printf("%c\n",dv1);
-            printf("%c\n",dv2);
             return printf("CPF INVALIDO\n");
         }
         else if ((dv1 >= 2 && cpf[12] == '0') || (dv2 >= 2 && cpf[13] == '0'))
@@ -69,8 +70,6 @@ char verificar(char *cpf, int tam)
         
         else if ((dv1 >= 2 && cpf[12] != dc1) || (dv2 >= 2 && cpf[13] != dc2))
         {
-            printf("%c\n",dc1);
-            printf("%c\n",dc2);
             return printf("CPF INVALIDO\n");
         } 
         else{
@@ -89,6 +88,9 @@ int main(int argc, char const *argv[])
     scanf("%s", cpf);
     getchar();
     int tam = strlen(cpf);
+    printf("%d", tam);
+    printf("%c", cpf[tam - 2]);
+    printf("%c", cpf[tam - 1]);
     verificar(cpf, tam);
 
     return 0;
