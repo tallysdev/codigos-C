112f01088ebc9b7d449f112d0b33cc7ae4790745
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-16 16:26:25 -0300

    validando calculo com char especiais

--- cpf-vali.c
+++ cpf-vali.c
@@ -28,9 +28,9 @@ char verificar(char *cpf, int tam)
     {
         /* code */
         int calc = 0;
-        calc = cpf[0] * 10 + cpf[1] * 9 + cpf[2] * 8 + cpf[3] * 7 + cpf[4] * 6 + cpf[5] * 5 + cpf[6] * 4 + cpf[7] * 3 + cpf[8] * 2;
+        calc = (cpf[0] - 48 )* 10 + (cpf[1] - 48 )* 9 + (cpf[2] - 48 )* 8 + (cpf[3] - 48 )* 7 + (cpf[4] - 48 )* 6 + (cpf[5] - 48 )* 5 + (cpf[6] - 48 )* 4 + (cpf[7] - 48 )* 3 + (cpf[8] - 48 )* 2;
         int dv1 = calc % 11;
-        int dv2 = (cpf[0] * 11 + cpf[1] * 10 + cpf[2] * 9 + cpf[3] * 8 + cpf[4] * 7 + cpf[5] * 6 + cpf[6] * 5 + cpf[7] * 4 + cpf[8] * 3 + cpf[9] * 2) % 11;
+        int dv2 = ((cpf[0] - 48 )* 11 + (cpf[1] - 48 )* 10 + (cpf[2] - 48 )* 9 + (cpf[3] - 48 )* 8 + (cpf[4] - 48 )* 7 + (cpf[5] - 48 )* 6 + (cpf[6] - 48 )* 5 + (cpf[7] - 48 )* 4 + (cpf[8] - 48 )* 3 + (cpf[9] - 48 )* 2) % 11;
 
         if ((dv1 < 2 && cpf[tam - 2] != '0') || (dv2 < 2 && cpf[tam - 2] != '0'))
         {
@@ -50,14 +50,16 @@ char verificar(char *cpf, int tam)
     else if (tam > 11)
     {
         int calc = 0;
-        calc = cpf[0] * 10 + cpf[1] * 9 + cpf[2] * 8 + cpf[4] * 7 + cpf[5] * 6 + cpf[6] * 5 + cpf[8] * 4 + cpf[9] * 3 + cpf[10] * 2;
+        calc = (cpf[0] - 48) * 10 + (cpf[1] - 48) * 9 + (cpf[2] - 48) * 8 + (cpf[4] - 48) * 7 + (cpf[5] - 48) * 6 + (cpf[6] - 48) * 5 + (cpf[8] - 48) * 4 + (cpf[9] - 48) * 3 + (cpf[10] - 48) * 2;
         int dv1 = calc % 11;
-        int dv2 = (cpf[0] * 11 + cpf[1] * 10 + cpf[2] * 9 + cpf[4] * 8 + cpf[5] * 7 + cpf[6] * 6 + cpf[8] * 5 + cpf[9] * 4 + cpf[10] * 3 + cpf[12] * 2) % 11;
+        int dv2 = ((cpf[0] - 48 )* 11 + (cpf[1] - 48 )* 10 + (cpf[2] - 48 )* 9 + (cpf[4] - 48 )* 8 + (cpf[5] - 48 )* 7 + (cpf[6] - 48 )* 6 + (cpf[8] - 48 )* 5 + (cpf[9] - 48 )* 4 + (cpf[10] - 48 )* 3 + (cpf[12] - 48 )* 2) % 11;
         char dc1, dc2; 
-        dc1 = (11 - dv1)+48;
-        dc2 = (11 - dv2)+48; 
+        dc1 = (11 - dv1) + 48;
+        dc2 = (11 - dv2) + 48; 
         if ((dv1 < 2 && cpf[12] != '0') || (dv2 < 2 && cpf[13] != '0'))
-        {
+        {   
+            printf("%c\n",dv1);
+            printf("%c\n",dv2);
             return printf("CPF a\n");
         }
         else if ((dv1 >= 2 && cpf[12] == '0') || (dv2 >= 2 && cpf[13] == '0'))
@@ -67,6 +69,8 @@ char verificar(char *cpf, int tam)
         
         else if ((dv1 >= 2 && cpf[12] != dc1) || (dv2 >= 2 && cpf[13] != dc2))
         {
+            printf("%c\n",dc1);
+            printf("%c\n",dc2);
             return printf("CPF c\n");
         } 
         else{
@@ -87,9 +91,11 @@ int main(int argc, char const *argv[])
     getchar();
     // printf("%s", cpf);
     int tam = strlen(cpf);
-    printf("%d", tam);
-    printf("%c\n", cpf[12]);
-    printf("%c\n", cpf[13]);
+    int z = (cpf[2] - 48) * 2;
+    printf("%d\n", z);
+    // printf("%d", tam);1
+    // printf("%c\n", cpf[12]);
+    // printf("%c\n", cpf[13]);
     verificar(cpf, tam);
 
     return 0;
