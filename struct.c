3d9f51fda212b42abf083d5ad3e97f6a22ddc3dc
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-25 18:16:40 -0300

    testando structs

--- struct.c
+++ struct.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct vacas Vacas;
 
 struct vacas
@@ -11,18 +10,84 @@ struct vacas
     float peso;
     char nome[12];
     char raca[12];
-
-
+    char status;
 };
 
-Vacas* cadastrarVacas();
-
-void mostrarVacas(const Vacas*);
-
-int menuzinho(void);
+Vacas *cadastrarVacas();
+void mostrarVacas(const Vacas* va);
+int menu(void);
 
 int main(int argc, char const *argv[])
 {
-    
+    Vacas *bovinos;
+
+    int op;
+    printf("Digite a opcao");
+    op = menu();
+    while (op != 0)
+    {
+        if (op == 1)
+        {
+            bovinos = cadastrarVacas();
+        }
+
+        else
+        {
+            mostrarVacas(bovinos);
+        }
+    }
+
     return 0;
+}
+
+Vacas *cadastrarVacas()
+{
+    Vacas *vac;
+    vac = (Vacas*)malloc(sizeof(Vacas));
+    printf("\nInforme o tipo da vaca: ");
+    scanf("%s", vac->tipo);
+    printf("Informe o peso da vaca: ");
+    scanf(" %f", &vac->peso);
+    printf("Informe o nome: ");
+    scanf(" %50[^\n]", vac->nome);
+    printf("Informe o login do vauno: ");
+    scanf(" %20[^\n]", vac->raca);
+    vac->status = 'c';
+    return vac;
+}
+
+void mostrarVacas(const Vacas* va)
+{
+    char situacao[20];
+    printf("\n= = = Vaca Cadastrada = = =\n");
+    printf("Tipo da vaca: %s\n", va->tipo);
+    printf("Peso da vaca: %f\n", va->peso);
+    printf("Nome da vaca: %s\n", va->nome);
+    printf("Raça da vaca: %s\n", va->raca);
+    if (va->status == 'c')
+    {
+        strcpy(situacao, "Cadastrada");
+    }
+    else if (va->status == 'a')
+    {
+        strcpy(situacao, "Abatida");
+    }
+    else
+    {
+        strcpy(situacao, "Vaca roubada");
+    }
+    printf("Situação da vaquinha: %s\n", situacao);
+}
+
+int menu(void)
+{
+    int op;
+    printf("\nMenu Principal\n");
+    printf("1 - Cadastrar Vaca\n");
+    printf("2 - Pesquisar Vaca\n");
+    printf("3 - Exibir Vaca\n");
+    printf("0 - Encerrar programa\n");
+    printf("Escolha sua opção: ");
+    scanf("%d", &op);
+    return op;
 }
