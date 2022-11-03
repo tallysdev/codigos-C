5eb2e1c12c7c63a5b0621319236a807c2b695f90
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-11-03 11:00:33 -0300

    add loop do menu

--- learning-file/mu.c
+++ learning-file/mu.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mu.h"
 
 void menu (void);
+void cadastrar (void);
+void listar(void);
 
 // animal = (Pet*) malloc(sizeof(Pet));
 // Mu* bovinos;
 
 int main(int argc, char const *argv[])
 {
+    int op;
+    menu();
+    scanf("%d", &op);
+    getchar();
+    while (op)
+    {
+        if (op == 1)
+        {
+            printf("teste 1\n");
+        }
+        else if (op == 2)
+        {
+            printf("teste 2\n");
+        }
+        else {
+            printf("\n\t opção invalida \n");
+            menu();
+            scanf("%d", &op);
+            getchar();
+        }        
+        
+        menu();
+        scanf("%d", &op);
+        getchar();
+    }
     
+
     return 0;
 }
 
@@ -18,4 +47,5 @@ void menu (void) {
     printf("Escolha a funçao quer voce quer fazer\n");
     printf("\n Cadastrar: 1\n");
     printf("\n Listas todos: 2\n");
+    printf("\n Sair: 0\n");
 }
