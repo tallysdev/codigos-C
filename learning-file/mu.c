bfd04467096504b6c8b2c225a405bbd47b9bbfa5
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-11-03 09:49:34 -0300

    criando menu

---
+++ learning-file/mu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mu.h"
+
+void menu (void);
+
+// animal = (Pet*) malloc(sizeof(Pet));
+// Mu* bovinos;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void menu (void) {
+    printf("Cadastrar Bovinos\n");
+    printf("Escolha a funçao quer voce quer fazer\n");
+    printf("\n Cadastrar: 1\n");
+    printf("\n Listas todos: 2\n");
+}
